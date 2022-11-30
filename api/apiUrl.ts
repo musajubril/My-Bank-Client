ea83ffda7c4d370e48984070d6562396a7aac5d2
Jubril Musa <41811898+musajubril@users.noreply.github.com>
2022-11-30 05:13:22 -0800

    Update apiUrl.ts

--- api/apiUrl.ts
+++ api/apiUrl.ts
@@ -1,6 +1,6 @@
 
 
-const API_URL = process.env.NODE_ENV==="production" ? "https://my-new-bank-api.herokuapp.com" : "http://localhost:4000";
+const API_URL = process.env.NODE_ENV==="production" ? "https://my-bank-api.onrender.com" : "http://localhost:4000";
 export const TRANSACTIONS = `${API_URL}/transactions`
 export const DEPOSIT = `${API_URL}/deposit`
 export const WITHDRAWAL = `${API_URL}/withdrawal`
